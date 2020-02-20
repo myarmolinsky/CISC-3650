ee61e1f737ee32785a85e6b2c1dd2a7aa1b212b7
myarmolinsky <myarmolinsky3043@bths.edu>
2020-02-20 15:37:08 -0500

    Completed Sections 2 and 3 of the project

--- Frontend Lab 1/JavaScript/script.js
+++ Frontend Lab 1/JavaScript/script.js
@@ -12,18 +12,42 @@ function drop(event) {
   event.target.appendChild(document.getElementById(data));
 }
 
+let valid = false;
 function checkSubmit() {
   console.log("worked");
+  valid = checkPasswords();
   if (
     document.getElementById("dragPokemonDiv1").firstElementChild !== null &&
     document.getElementById("dragPokemonDiv1").firstElementChild.id ===
-      "pokemon2"
+      "pokemon2" &&
+    valid
   ) {
     console.log("enabled");
     document.getElementById("submitButton").disabled = false;
   } else {
     console.log("disabled");
     document.getElementById("submitButton").disabled = "disabled";
+  }
+}
+
+function checkPasswords() {
+  console.log("called");
+  if (
+    document.getElementById("favPoke1").value != "" &&
+    document.getElementById("favPoke1").value ==
+      document.getElementById("favPoke2").value
+  ) {
+    document.getElementById("favPoke1").style = "background-color: green;";
+    document.getElementById("favPoke2").style = "background-color: green;";
+    document.getElementById("passwordsMatch").innerHTML =
+      "The Pokemon you have entered match!";
+    return true;
+  } else {
+    document.getElementById("favPoke1").style = "background-color: red;";
+    document.getElementById("favPoke2").style = "background-color: red;";
+    document.getElementById("passwordsMatch").innerHTML =
+      "The Pokemon you have entered don't match, they must match to be able to submit this form";
+    return false;
   }
 }
 
@@ -68,4 +92,28 @@ function toggleAudio() {
   } else {
     document.getElementById("audioDiv").innerHTML = "";
   }
+}
+
+var theme = true;
+function toggleTheme() {
+  theme = !theme;
+  if (theme) {
+    document.getElementById("theme").innerHTML =
+      "You are currently using light theme, click the button below to switch to dark theme";
+    document.body.style = "background-color: skyblue;";
+    document.getElementById("theme").style =
+      "background-color: cornflowerblue; border-radius: 5px; display: inline;";
+  } else {
+    document.getElementById("theme").innerHTML =
+      "You are currently using dark theme, click the button below to switch to light theme";
+    document.body.style = "background-color: black; color: lightgray";
+    document.getElementById("theme").style =
+      "background-color: navy; border-radius: 5px; display: inline;";
+  }
+}
+
+function checkRating() {
+  document.getElementById("rating").innerHTML = document.getElementById(
+    "rater"
+  ).value;
 }
